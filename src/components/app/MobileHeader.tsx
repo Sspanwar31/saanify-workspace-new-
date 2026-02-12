c961db5bbd5d435174e9c8dbc8b11f1517b8834e
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-02-12 21:31:58 +0530

    Create MobileHeader.tsx

---
+++ src/components/app/MobileHeader.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function MobileHeader() {
+  return (
+    <div className="h-14 flex items-center px-4 bg-background border-b">
+      <span className="font-semibold">Saanify</span>
+    </div>
+  );
+}
