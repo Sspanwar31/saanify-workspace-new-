eb68504d3b34cc3d9f66ffbc551fd38ad5b869e7
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-02-12 21:39:00 +0530

    Update DesktopShell.tsx

--- src/components/app/DesktopShell.tsx
+++ src/components/app/DesktopShell.tsx
@@ -1,3 +1,15 @@
 'use client';
 
-import Sidebar from '@/components/Sidebar';
+export default function DesktopShell({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen w-full">
+      <main className="flex-1 overflow-auto">
+        {children}
+      </main>
+    </div>
+  );
+}
