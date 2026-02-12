bbabd2478e541ea7395f09438a8b28d93417af3f
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-02-12 21:23:39 +0530

    Create MobileShell.tsx

---
+++ src/components/app/MobileShell.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import MobileHeader from './MobileHeader';
+import MobileDrawer from './MobileDrawer';
+
+export default function MobileShell({ children }) {
+  return (
+    <div className="h-screen w-full bg-background overflow-hidden">
+      <MobileHeader />
+      <MobileDrawer />
+      <main className="pt-14 h-full overflow-auto">
+        {children}
+      </main>
+    </div>
+  );
+}
