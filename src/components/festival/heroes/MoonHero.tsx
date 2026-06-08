3f4ac87f56aadf6c9b969a0db76bab92791807b4
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:37:32 +0530

    Create MoonHero.tsx

---
+++ src/components/festival/heroes/MoonHero.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function MoonHero() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-[120px]">
+        🌙
+      </div>
+
+      <div className="mt-4 text-emerald-400 font-bold text-xl">
+        Eid Mubarak
+      </div>
+    </div>
+  );
+}
