0622165ad9e598fd6dd4af82baff25c413745b74
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:21:39 +0530

    Update FestivalAnimationEngine.tsx

--- src/components/festival/FestivalAnimationEngine.tsx
+++ src/components/festival/FestivalAnimationEngine.tsx
@@ -1,24 +1,31 @@
+import GoldenParticles from './animations/GoldenParticles';
+import ColorSplash from './animations/ColorSplash';
+import SnowFall from './animations/SnowFall';
+import MoonGlow from './animations/MoonGlow';
+import TricolorWaves from './animations/TricolorWaves';
+
 export default function FestivalAnimationEngine({
   animationTheme
-}:{
-  animationTheme:string
+}: {
+  animationTheme: string;
 }) {
-
-  switch(animationTheme){
-
+  switch (animationTheme) {
     case 'GOLDEN_PARTICLES':
-      return <GoldenParticles />
+      return <GoldenParticles />;
 
     case 'COLOR_SPLASH':
-      return <ColorSplash />
+      return <ColorSplash />;
 
     case 'SNOW_FALL':
-      return <SnowFall />
+      return <SnowFall />;
 
     case 'MOON_GLOW':
-      return <MoonGlow />
+      return <MoonGlow />;
+
+    case 'TRICOLOR_WAVES':
+      return <TricolorWaves />;
 
     default:
-      return null
+      return null;
   }
 }
