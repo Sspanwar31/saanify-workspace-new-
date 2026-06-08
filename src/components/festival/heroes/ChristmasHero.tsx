06fd4857f9f33a0d40b2fb61f81169dae875f8e9
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:38:16 +0530

    Create ChristmasHero.tsx

---
+++ src/components/festival/heroes/ChristmasHero.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function ChristmasHero() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-[120px]">
+        🎄
+      </div>
+
+      <div className="mt-4 text-green-400 font-bold text-xl">
+        Merry Christmas
+      </div>
+    </div>
+  );
+}
