9eca7dee063dc976df3b2b79ab9e4d52ab2bd121
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:39:32 +0530

    Update FestivalHeroEngine.tsx

--- src/components/festival/FestivalHeroEngine.tsx
+++ src/components/festival/FestivalHeroEngine.tsx
@@ -1,21 +1,33 @@
+import RoyalDiya from './heroes/RoyalDiya';
+import GaneshaHero from './heroes/GaneshaHero';
+import DurgaHero from './heroes/DurgaHero';
+import MoonHero from './heroes/MoonHero';
+import ChristmasHero from './heroes/ChristmasHero';
+
 export default function FestivalHeroEngine({
- heroVisual
-}:{
- heroVisual:string
-}){
+  heroVisual
+}: {
+  heroVisual: string;
+}) {
 
- switch(heroVisual){
+  switch(heroVisual){
 
-   case 'ROYAL_DIYA':
-      return <RoyalDiya/>
+    case 'ROYAL_DIYA':
+      return <RoyalDiya />;
 
-   case 'MAA_DURGA':
-      return <DurgaHero/>
+    case 'GANESHA':
+      return <GaneshaHero />;
 
-   case 'CRESCENT_MOON':
-      return <MoonHero/>
+    case 'MAA_DURGA':
+      return <DurgaHero />;
 
-   default:
-      return null
- }
+    case 'CRESCENT_MOON':
+      return <MoonHero />;
+
+    case 'CHRISTMAS_TREE':
+      return <ChristmasHero />;
+
+    default:
+      return null;
+  }
 }
