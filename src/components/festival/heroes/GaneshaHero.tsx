43e83a5e59a66359ae6e6efb56ae8597c4145f04
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:35:34 +0530

    Create GaneshaHero.tsx

---
+++ src/components/festival/heroes/GaneshaHero.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function GaneshaHero() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-[120px]">
+        🐘
+      </div>
+
+      <div className="mt-4 text-orange-400 font-bold text-xl">
+        Ganpati Bappa Morya
+      </div>
+    </div>
+  );
+}
