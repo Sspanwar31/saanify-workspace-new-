9ca62a33970cfd409205c65a637623296528e9a3
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:36:56 +0530

    Create DurgaHero.tsx

---
+++ src/components/festival/heroes/DurgaHero.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function DurgaHero() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-[120px]">
+        🔱
+      </div>
+
+      <div className="mt-4 text-red-400 font-bold text-xl">
+        Jai Maa Durga
+      </div>
+    </div>
+  );
+}
