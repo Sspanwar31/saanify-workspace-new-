a057ebaefe8621dff1c251ac993813ea1a9fab69
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-08 22:34:56 +0530

    Create RoyalDiya.tsx

---
+++ src/components/festival/heroes/RoyalDiya.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function RoyalDiya() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="text-[120px] animate-pulse">
+        🪔
+      </div>
+
+      <div className="mt-4 text-yellow-400 font-bold text-xl">
+        Festival of Lights
+      </div>
+    </div>
+  );
+}
