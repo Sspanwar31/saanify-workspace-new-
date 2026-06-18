9de36844c44924213b23b6577022b1526f806d31
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-18 22:47:24 +0530

    Create heroRegistry.ts

---
+++ src/config/heroRegistry.ts
@@ -0,0 +1,49 @@
+export const HeroRegistry = {
+  // FESTIVALS
+  ROYAL_DIYA: RoyalDiya,
+  ROYAL_GANESHA: GaneshaHero,
+  GANESHA: GaneshaHero,
+
+  MAA_DURGA: DurgaHero,
+  DIVINE_TRISHUL: DurgaHero,
+
+  VIBRANT_PALETTE: HoliPalette,
+
+  ASHOKA_CHAKRA: AshokaChakra,
+  DHARMA_CHAKRA: AshokaChakra,
+
+  CHRISTMAS_TREE: ChristmasHero,
+  XMAS_TREE: ChristmasHero,
+
+  MOON_HERO: MoonHero,
+  CRESCENT_MOON: MoonHero,
+  EID_MUBARAK: MoonHero,
+
+  GANGA_GHAT_DIYA: RoyalDiya,
+
+  DANDIYA_BEAT: DandiyaHero,
+  RAVAN_DAHAN: RavanHero,
+  BABY_KRISHNA: KrishnaHero,
+  BROTHER_BOND: RakhiHero,
+  KITES_FLYING: KiteHero,
+  REAL_BONFIRE: BonfireHero,
+  SHIVA_POWER: ShivaHero,
+  RAM_DHARMA: RamHero,
+  HANUMAN_GADA: HanumanHero,
+  MOON_SIEVE: KarwaHero,
+  SUN_ARGHYA: ChhathHero,
+  HARVEST_POT: PongalHero,
+  HOLY_KAABA: KaabaHero,
+  NY_COUNTDOWN: NewYearHero,
+  NATIONAL_PRIDE: IndiaHero,
+  SIKH_KHANDA: SikhHero,
+  HEART_COUPLE: ValentineHero,
+
+  // CORPORATE
+  MEGAPHONE: AnnouncementHero,
+  SIREN: EmergencyHero,
+  GEAR_ICON: SystemHero,
+  TOOLS_ICON: MaintenanceHero,
+  GIFT_BOX: OfferHero,
+  CALENDAR_STAR: EventHero,
+};
