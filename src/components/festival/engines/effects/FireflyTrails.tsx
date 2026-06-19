0f5b356d488df3636f667ca60ea8ccc0537f8e9a
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-19 18:54:57 +0530

    Create FireflyTrails.tsx

---
+++ src/components/festival/engines/effects/FireflyTrails.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+export default function FireflyTrails() {
+  return (
+    <>
+      <div className="absolute top-20 left-1/4 text-yellow-300">
+        ✨
+      </div>
+
+      <div className="absolute top-1/2 right-20 text-yellow-300">
+        ✨
+      </div>
+
+      <div className="absolute bottom-32 left-1/2 text-yellow-300">
+        ✨
+      </div>
+    </>
+  );
+}
