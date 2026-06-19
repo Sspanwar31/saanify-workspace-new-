57a24baa847c0e855008fa48e6445190f230514c
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-19 20:30:50 +0530

    Update DevDeepawaliScene.tsx

--- src/components/festival/engines/presets/DevDeepawaliScene.tsx
+++ src/components/festival/engines/presets/DevDeepawaliScene.tsx
@@ -2,10 +2,28 @@
 
 import GoldenParticles from '../../animations/GoldenParticles';
 
+import LuxuryGlow from '../effects/LuxuryGlow';
+import LuxuryRays from '../effects/LuxuryRays';
+import FloatingDiyas from '../effects/FloatingDiyas';
+import FireflyTrails from '../effects/FireflyTrails';
+import RiverReflection from '../effects/RiverReflection';
+
 export default function DevDeepawaliScene() {
   return (
-    <div className="absolute inset-0 pointer-events-none">
+    <div className="absolute inset-0 overflow-hidden pointer-events-none">
+
+      <LuxuryGlow />
+
+      <LuxuryRays />
+
       <GoldenParticles preset="DEV_DEEPAWALI" />
+
+      <FloatingDiyas />
+
+      <FireflyTrails />
+
+      <RiverReflection />
+
     </div>
   );
 }
