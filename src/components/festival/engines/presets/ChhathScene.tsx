48add59456d3c083d7b438d085d8ebef1ef72f83
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-19 18:17:17 +0530

    Create ChhathScene.tsx

---
+++ src/components/festival/engines/presets/ChhathScene.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import GoldenParticles from '../../animations/GoldenParticles';
+
+export default function ChhathScene() {
+  return (
+    <div className="absolute inset-0 pointer-events-none">
+      <GoldenParticles preset="CHHATH" />
+    </div>
+  );
+}
