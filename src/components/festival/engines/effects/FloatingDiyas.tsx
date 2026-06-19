02da9ab7f427d2bef4b6dc4c013b0021e46c66eb
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-19 18:54:09 +0530

    Create FloatingDiyas.tsx

---
+++ src/components/festival/engines/effects/FloatingDiyas.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+export default function FloatingDiyas() {
+  return (
+    <>
+      <div
+        className="absolute bottom-10 left-10 text-5xl animate-pulse"
+      >
+        🪔
+      </div>
+
+      <div
+        className="absolute bottom-20 right-20 text-4xl animate-pulse"
+      >
+        🪔
+      </div>
+
+      <div
+        className="absolute top-40 right-1/4 text-3xl animate-pulse"
+      >
+        🪔
+      </div>
+    </>
+  );
+}
