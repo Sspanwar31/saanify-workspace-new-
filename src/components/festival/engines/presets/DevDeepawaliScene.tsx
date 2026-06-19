e5e6139514aad76f5262ace7f763d552f16fd358
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-19 18:15:42 +0530

    Create DevDeepawaliScene.tsx

---
+++ src/components/festival/engines/presets/DevDeepawaliScene.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import GoldenParticles from '../../animations/GoldenParticles';
+
+export default function DevDeepawaliScene() {
+  return (
+    <div className="absolute inset-0 pointer-events-none">
+      <GoldenParticles preset="DEV_DEEPAWALI" />
+    </div>
+  );
+}
