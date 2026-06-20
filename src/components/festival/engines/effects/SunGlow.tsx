825e9ba5f750ba7355d523ed63c9830f7c819c15
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-20 18:48:38 +0530

    Update SunGlow.tsx

--- src/components/festival/engines/effects/SunGlow.tsx
+++ src/components/festival/engines/effects/SunGlow.tsx
@@ -2,17 +2,31 @@
 
 export default function SunGlow() {
   return (
-    <div
-      style={{
-        position: 'absolute',
-        top: '100px',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        width: '300px',
-        height: '300px',
-        background: 'red',
-        zIndex: 9999,
-      }}
-    />
+    <>
+      {/* Core Sun */}
+      <div
+        className="absolute left-1/2 top-[6%] -translate-x-1/2 rounded-full"
+        style={{
+          width: '120px',
+          height: '120px',
+          background:
+            'radial-gradient(circle, #FFE28A 0%, #FFC857 70%, #FFB347 100%)',
+          zIndex: 2,
+        }}
+      />
+
+      {/* Outer Glow */}
+      <div
+        className="absolute left-1/2 top-[2%] -translate-x-1/2 rounded-full"
+        style={{
+          width: '320px',
+          height: '320px',
+          background:
+            'radial-gradient(circle, rgba(255,220,120,.8) 0%, rgba(255,180,60,.4) 45%, transparent 80%)',
+          filter: 'blur(35px)',
+          zIndex: 1,
+        }}
+      />
+    </>
   );
 }
