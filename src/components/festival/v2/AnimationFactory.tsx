23199c57f49c4f2564c1c8a05e4e7969393f8f74
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-28 22:02:35 +0530

    Update AnimationFactory.tsx

--- src/components/festival/v2/AnimationFactory.tsx
+++ src/components/festival/v2/AnimationFactory.tsx
@@ -5,32 +5,26 @@ import { AnimationRegistry } from '@/config/AnimationRegistry';
 export default function AnimationFactory({
   engine,
   preset,
+  phase,
 }: {
   engine?: string;
   preset?: string;
+  phase?: string;
 }) {
 
-  console.log('=== AnimationFactory ===');
-  console.log('engine =>', engine);
-  console.log('preset =>', preset);
-
-  if (!engine) {
-    console.warn('NO ENGINE RECEIVED');
-    return null;
-  }
+  if (!engine) return null;
 
   const Engine =
     AnimationRegistry[
       engine as keyof typeof AnimationRegistry
     ];
 
-  console.log('Engine Component =>', Engine);
+  if (!Engine) return null;
 
-  if (!Engine) {
-    console.error('ENGINE NOT FOUND =>', engine);
-    console.log('Available Engines =>', Object.keys(AnimationRegistry));
-    return null;
-  }
-
-  return <Engine preset={preset} />;
+  return (
+    <Engine
+      preset={preset}
+      phase={phase}
+    />
+  );
 }
