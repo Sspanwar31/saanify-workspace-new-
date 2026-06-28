628b169cf41e8ba126f804271dc87d8427003c57
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-28 20:29:49 +0530

    Update DiwaliScene.tsx

--- src/components/festival/engines/presets/DiwaliScene.tsx
+++ src/components/festival/engines/presets/DiwaliScene.tsx
@@ -5,6 +5,9 @@ import GoldenParticles from '../../animations/GoldenParticles';
 import LuxuryGlow from '../effects/LuxuryGlow';
 import BloomLighting from '../effects/BloomLighting';
 
+import RocketLaunch from '../effects/RocketLaunch';
+import FireworkBurst from '../effects/FireworkBurst';
+
 export default function DiwaliScene() {
   return (
     <div className="absolute inset-0 overflow-hidden pointer-events-none">
@@ -12,8 +15,16 @@ export default function DiwaliScene() {
       {/* Premium Ambient Glow */}
       <LuxuryGlow />
 
+      <LuxuryRays />
+
       {/* Soft Bloom Lighting */}
       <BloomLighting />
+
+      {/* Festival Entry */}
+       <RocketLaunch />
+
+    {/* Firework Celebration */}
+      <FireworkBurst />
 
       {/* Golden Festival Atmosphere */}
       <GoldenParticles preset="DIWALI" />
