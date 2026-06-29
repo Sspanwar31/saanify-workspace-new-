fa573e7a683b3287e1eea073669ee11dbb381b4e
Sspanwar31 <s.s.panwar2531@gmail.com>
2026-06-29 19:07:21 +0530

    Update SpiritualEngine.tsx

--- src/components/festival/engines/SpiritualEngine.tsx
+++ src/components/festival/engines/SpiritualEngine.tsx
@@ -3,6 +3,8 @@
 import DiwaliScene from './presets/DiwaliScene';
 import DevDeepawaliScene from './presets/DevDeepawaliScene';
 
+import FestivalIntroController from '../intro/FestivalIntroController';
+
 export default function SpiritualEngine({
   preset,
   phase,
@@ -16,9 +18,31 @@ export default function SpiritualEngine({
   switch (preset) {
 
     case 'DIWALI':
+
+      // INTRO MODE
+      if (phase === 'INTRO') {
+        return (
+          <FestivalIntroController>
+            {(introPhase) => {
+              console.log(
+                'INTRO PHASE =',
+                introPhase
+              );
+
+              return (
+                <DiwaliScene
+                  phase={introPhase}
+                />
+              );
+            }}
+          </FestivalIntroController>
+        );
+      }
+
+      // ACTIVE MODE
       return (
         <DiwaliScene
-          phase={phase}
+          phase="AMBIENT"
         />
       );
 
